bfcb45448033a1896fa7abf3bed6f8281882c72f
Francesca Bagordo <bagordofrancesca587@gmail.com>
2023-12-09 21:31:29 +0100

    implementazione Footer.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import {NavBar} from "./componets/NavBar";
+import {Footer} from "./componets/Footer";
 function App() {
     return (
             <div className="App">
                 <NavBar></NavBar>
+                <Footer/>
             </div>
     );
 }
