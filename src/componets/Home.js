b965180fd43c643e09c5f036bd1261ebc9554230
Francesca Bagordo <bagordofrancesca587@gmail.com>
2023-12-12 21:00:11 +0100

    stilizzata home

--- src/componets/Home.js
+++ src/componets/Home.js
@@ -5,14 +5,13 @@ export function Home() {
     return (
         <div>
             <section className="services">
-                <h2>I nostri Servizi</h2>
-                <p>
+                <p className="services-description">
                     Offriamo un'ampia gamma di servizi di noleggio con conducente per rendere
                     il tuo viaggio comodo e indimenticabile. Dai trasferimenti per aeroporti
                     a tour turistici personalizzati e servizi aziendali, siamo qui per soddisfare
                     ogni tua esigenza di trasporto.
                 </p>
-                <ul>
+                <ul className="services-list">
                     <li>Trasferimenti per aeroporti con accoglienza personalizzata</li>
                     <li>Tour turistici guidati alla scoperta dei luoghi più affascinanti</li>
                     <li>Servizi aziendali per incontri e trasferimenti esecutivi</li>
