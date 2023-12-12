3eb237ac2165a07cadec394a0160dd5a69b74719
Francesca Bagordo <bagordofrancesca587@gmail.com>
2023-12-12 19:37:18 +0100

    implementazione Home.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import {NavBar} from "./componets/NavBar";
 import {Footer} from "./componets/Footer";
+import {Home} from "./componets/Home";
+
 function App() {
     return (
-            <div className="App">
+            <div className="app-container">
                 <NavBar></NavBar>
+                <Home/>
                 <Footer/>
             </div>
     );
